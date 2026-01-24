4d6ce7855e614890e5f75da9a9ad3f3a72c000f1
Konstantinos Karatsioris <konstantinoskaratsioris@iMac.fritz.box>
2026-01-25 00:12:55 +0100

    first try create login page with Svelte framework

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,1 +1,37 @@
-<h1>Hello World</h1>
+<header>
+
+</header>
+
+<main>
+  <h1 id="status">{moodStatus}</h1>
+  <div id="square"></div>
+  <button id="mood-btn" on:click={handleMoodClick}>Change Mood</button> 
+</main>
+
+
+<footer>
+  <div id="header"> </div>
+</footer>
+
+<script>
+
+  let moodStatus = "The mood is calm";
+  let isParty = false;
+
+  function handleMoodClick()
+  {
+    isParty = true;
+    moodStatus = "The mood is Party! 🎉";
+  }
+
+  function handleOutsideClick(event)
+  {
+    if (event.target.id !== 'mood-btn')
+    {
+      isParty = false;
+      moodStatus = "The mood is calm";
+    }
+  }
+</script>
+
+<svelte:window on:click={handleOutsideClick} />
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,8 @@
 import { mount } from 'svelte'
-import './app.css'
+// import './app.css'
+import './style.css' //my style file
 import App from './App.svelte'
 
-const app = mount(App, {
-  target: document.getElementById('app'),
-})
+const app = mount(App, {target: document.getElementById('app'),})
 
-export default app
+export default app  
