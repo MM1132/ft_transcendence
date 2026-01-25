a5300112a3df0b99eeda22be8fedd9ece0b028b1
Konstantinos Karatsioris <konstantinoskaratsioris@iMac.fritz.box>
2026-01-25 20:59:49 +0100

    change the login-form with classes and create a sample of logo

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,22 +1,28 @@
 <header>
   <div id="header">
     <div class="header-logo">
-      <img src="src/logo-rm.png" alt="Logo"/>
+      <img src="src/c.svg" alt="Logo"/>
     </div>
      <div class="header-nav">
-      <a href="#leaderboard">Leaderboard</a>
-      <a href="#settings">Settings</a>
+      <a href="#leaderboard">LEADERBOARD</a>
+      <a href="#settings">SETTINGS</a>
     </div>
   </div>
 </header>
 
 <main>
-  <!-- <h1 id="status">{moodStatus}</h1> -->
-  <div id="square"></div>
-  <!-- <button id="login-btn" on:click={handleMoodClick}>Change Mood</button> -->
+  <div id="logo">LOGIN</div>
+  <div id="border"></div>
   <div id="login-form">
-    <input type="text" id="username" placeholder="Username" />
-    <input type="text" id="password" placeholder="Password" />
+    <div class="input-group">
+      <p>USERNAME</p>
+      <input type="text" id="username" placeholder="Username" />
+    </div>
+    <div class="input-group">
+      <p>PASSWORD</p>
+      <input type="text" id="password" placeholder="Password" />
+    </div>
+    <!-- <button id="login-btn" on:click={handleMoodClick}>Change Mood</button> -->
     <button id="login-btn">Login</button>
   </div>
 </main>
@@ -30,23 +36,7 @@
 
 <script>
 
-  let moodStatus = "The mood is calm";
-  let isParty = false;
-
-  function handleMoodClick()
-  {
-    isParty = true;
-    moodStatus = "The mood is Party! 🎉";
-  }
-
-  function handleOutsideClick(event)
-  {
-    if (event.target.id !== 'mood-btn')
-    {
-      isParty = false;
-      moodStatus = "The mood is calm";
-    }
-  }
+  
 </script>
 
-<svelte:window on:click={handleOutsideClick} />
+<!-- <svelte:window on:click={handleOutsideClick} /> -->
