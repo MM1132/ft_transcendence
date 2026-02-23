8d2973f13e72588a43fdd1ed8d026ccf603c4331
Robert Reimann <34074906+MM1132@users.noreply.github.com>
2026-02-23 19:35:52 +0100

    Fix BACKEND_ROOT_PATH and allow origin http://localhost:8080

--- backend/src/init/registerPlugins.ts
+++ backend/src/init/registerPlugins.ts
@@ -17,7 +17,7 @@ export const registerPlugins = (fastify: FastifyInstance) => {
   });
 
   fastify.register(cors, {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://localhost:8080'],
     credentials: true,
     exposedHeaders: ['Authorization', 'Content-type'],
   });
